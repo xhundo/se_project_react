f83cefc9d8e8efe93e9b9fc16b1e5add07c3c6f7
Kurtney Joseph <josephkurt18@gmail.com>
2022-10-13 21:12:24 -0400

    make corrections & modify modal

--- src/components/AddItemModal.js
+++ src/components/AddItemModal.js
@@ -1,5 +1,5 @@
 import ModalWithForm from "./ModalWithForm";
-
+import React, { useState, useEffect } from "react";
 function AddItemModal({
   isOpen,
   closeModal,
@@ -7,6 +7,32 @@ function AddItemModal({
   closeByTarget,
   onAddItem,
 }) {
+  const [name, setName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    setName("");
+    setImageUrl("");
+  }, [isOpen]);
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleImageChange = (e) => {
+    setImageUrl(e.target.value);
+  };
+
+  const handleWeather = (e) => {
+    setWeather(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddItem({ name, weather, imageUrl });
+    closeModal();
+  };
   {
     return (
       <ModalWithForm
@@ -18,7 +44,71 @@ function AddItemModal({
         closeByEsc={closeByEsc}
         closeModal={closeByTarget}
         onAddItem={onAddItem}
-      />
+      >
+        <label className="modal__input-label">Name</label>
+        <input
+          className="modal__input"
+          type="text"
+          placeholder="Name"
+          value={name}
+          required
+          onChange={handleChangeName}
+        />
+        <label className="modal__input-label">Image</label>
+        <input
+          className="modal__input"
+          type="url"
+          value={imageUrl}
+          placeholder="Image URL"
+          required
+          onChange={handleImageChange}
+        />
+        <label className="modal__input-title">Select the weather type:</label>
+        <label className="modal__input-text" htmlFor="hot">
+          <input
+            className="modal__input-radio"
+            id="hot"
+            type="radio"
+            value="hot"
+            name={weather}
+            onChange={handleWeather}
+          />
+          Hot
+        </label>
+        <label className="modal__input-text" htmlFor="warm">
+          <input
+            className="modal__input-radio"
+            id="warm"
+            type="radio"
+            value="warm"
+            name={weather}
+            onChange={handleWeather}
+          />
+          Warm
+        </label>
+
+        <label className="modal__input-text" htmlFor="cold">
+          <input
+            className="modal__input-radio"
+            id="cold"
+            type="radio"
+            value="cold"
+            name={weather}
+            onChange={handleWeather}
+          />
+          Cold
+        </label>
+        <button
+          type="button"
+          className="modal__btn-close"
+          onClick={closeModal}
+        ></button>
+        <button
+          className="modal__submit"
+          type="submit"
+          onClick={handleSubmit}
+        ></button>
+      </ModalWithForm>
     );
   }
 }
